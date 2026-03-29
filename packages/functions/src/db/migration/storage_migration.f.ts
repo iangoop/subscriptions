97a9034be179ede7d8a74c44b4ee6611cbff2b56
Ian Gopfert <iangoop@gmail.com>
2026-03-29 11:22:53 +0100

    inconsistency between default bucket and functions region, getting bucket from parameter again so its possible to use a new one

--- packages/functions/src/db/migration/storage_migration.f.ts
+++ packages/functions/src/db/migration/storage_migration.f.ts
@@ -43,6 +43,7 @@ import { SubscriptionPayload } from '../types/subscriptions';
  */
 export const storageMigrationTrigger = onObjectFinalized(
   {
+    bucket: process.env.CUSTOM_FIREBASE_STORAGEBUCKET,
     cpu: 1,
     memory: '1GiB',
     timeoutSeconds: 540, // Increased timeout for large files
