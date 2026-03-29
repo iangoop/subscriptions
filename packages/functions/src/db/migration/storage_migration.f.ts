3b4d073e894d4fbcdb546b5cf1773ca778ab60b1
Ian Gopfert <iangoop@gmail.com>
2026-03-29 11:54:43 +0100

    still getting the same region error even though pointing to another bucket, trying to force set region

--- packages/functions/src/db/migration/storage_migration.f.ts
+++ packages/functions/src/db/migration/storage_migration.f.ts
@@ -44,6 +44,7 @@ import { SubscriptionPayload } from '../types/subscriptions';
 export const storageMigrationTrigger = onObjectFinalized(
   {
     bucket: process.env.CUSTOM_FIREBASE_STORAGEBUCKET,
+    region: process.env.CUSTOM_FIREBASE_REGION,
     cpu: 1,
     memory: '1GiB',
     timeoutSeconds: 540, // Increased timeout for large files
